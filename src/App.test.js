c88d220a877e646b5a59df24732525d7c4d2ba5f
Natividad Alvarez <tivialvarez@gmail.com>
2020-08-06 10:18:03 -0400

    Wrote test for checking validation of firstname

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import * as rtl from "@testing-library/react";
+
 import App from "./App";
 
-test("renders App without crashing", () => {
-  render(<App />);
-});
+  test("renders app", async ()=>{
+    const wrapper = rtl.render(<App/>);
+    const firstnameInput = wrapper.queryByTestId('firstname-input')
+    const submitButton = wrapper.queryByRole("button");
+
+    await rtl.act(async ()=>{
+      rtl.fireEvent.change(firstnameInput, {target: {value: "Samantha"}});
+      rtl.fireEvent.click(submitButton);
+    });
+
+    const firstnameError = wrapper.queryByTestId("firstname-error");
+
+    expect(firstnameError).not.toBeInTheDocument();
+  });
+
+
