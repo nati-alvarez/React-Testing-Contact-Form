2ff23a3a66c83ce165e253b94f5b869a081dbf00
Natividad Alvarez <tivialvarez@gmail.com>
2020-08-13 07:15:35 -0400

    Changed dom query methods

--- src/App.test.js
+++ src/App.test.js
@@ -5,12 +5,12 @@ import App from "./App";
 
   test("renders app", async ()=>{
     const wrapper = rtl.render(<App/>);
-    const firstnameInput = wrapper.queryByTestId('firstname-input')
-    const submitButton = wrapper.queryByRole("button");
+    const firstnameInput = wrapper.getByPlaceholderText('Edd');
+    const submitButton = wrapper.getByRole("button");
 
     await rtl.act(async ()=>{
       rtl.fireEvent.change(firstnameInput, {target: {value: "Samantha"}});
-      rtl.fireEvent.click(submitButton);
+      rtl.fireEvent.submit(submitButton);
     });
 
     const firstnameError = wrapper.queryByTestId("firstname-error");
